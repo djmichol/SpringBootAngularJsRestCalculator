ed19c56ed84e4cb112f3239143bd40023b770cc7
mpodbielski <mic.podbielski@wp.pl>
2017-11-10 13:59:46 +0100

    add send rest request to calculate

--- src/main/resources/static/js/calculatorController.js
+++ src/main/resources/static/js/calculatorController.js
@@ -1,7 +1,14 @@
-app.controller('calculatorController', function($scope) {
+app.controller('calculatorController', function($scope, $http) {
     $scope.operation;
+    $scope.result;
 
     $scope.submit = function(){
-        console.log( $scope.operation);
+        console.log($scope.operation);
+        var postData = {};
+        postData.operation =  $scope.operation;
+        $http.post('api/calculator', postData)
+        .then(function successCallback(response) {
+            $scope.result = response.data.result;
+        });
     }
 });
