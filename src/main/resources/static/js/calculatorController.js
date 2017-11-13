1f15854a17671d97d0a94e72d0049d5263bc4018
mpodbielski <mic.podbielski@wp.pl>
2017-11-13 15:36:42 +0100

    add calculator css on front

--- src/main/resources/static/js/calculatorController.js
+++ src/main/resources/static/js/calculatorController.js
@@ -1,5 +1,5 @@
 app.controller('calculatorController', function($scope, $http) {
-    $scope.operation;
+    $scope.operation = "";
     $scope.result;
 
     $scope.submit = function(){
@@ -9,6 +9,15 @@ app.controller('calculatorController', function($scope, $http) {
         $http.post('api/calculator', postData)
         .then(function successCallback(response) {
             $scope.result = response.data.result;
+            $scope.operation = response.data.result;
         });
+    }
+
+    $scope.addToOperation = function(cos){
+        $scope.operation += cos;
+    }
+
+    $scope.clear = function(){
+        $scope.operation = "";
     }
 });
